8de0feeed65976063b154f79419a048b88f07376
Alessio Bonti <alebonti@au1.ibm.com>
2016-09-15 09:20:31 +1000

    testing new buildpacks

--- extra/mongo_url.sh
+++ extra/mongo_url.sh
@@ -8,11 +8,6 @@
 #
 echo "-----> Adding profile script to resolve MONGO_URL from mongolab addon"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo_url.sh <<EOF
-  #!/bin/bash
-  if [ -z \$MONGO_URL ] ; then
-    export MONGO_URL=\${MONGODB_URI:-\${MONGOLAB_URI:-\$MONGOHQ_URL}}
-  fi
-  if [ -z \$MONGO_URL ] ; then
-    echo "meteor-buildpack-horse: MONGO_URL missing, you must define it for meteor to work."
-  fi
+  #!/bin/sh
+  export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['credentials']['url'].start_with? 'mongodb' }[1][0]['credentials']['url']"`}
 EOF
