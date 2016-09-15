e38c1c0c1279fa64c2a9a7367078548ab0cac01f
Alessio Bonti <alebonti@au1.ibm.com>
2016-09-15 10:26:20 +1000

    commit urls

--- extra/root_url.sh
+++ extra/root_url.sh
@@ -5,7 +5,7 @@
 echo "-----> Adding profile script to resolve ROOT_URL from heroku app name"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/root_url.sh <<EOF
   #!/bin/bash
-    export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['credentials']['url'].start_with? 'mongodb' }[1][0]['credentials']['url']"`}
+  export ROOT_URL=\${ROOT_URL:-`ruby -e 'require "json"; puts "http://#{JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"][0]}"'`}
 
   #if [ -z "\$ROOT_URL" ] && [ -n "\$HEROKU_APP_NAME" ] ; then
   #  export ROOT_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
