8cf536da629717e67d68164483feda87e47f8838
Alessio Bonti <alebonti@au1.ibm.com>
2016-09-15 10:09:39 +1000

    commit urls

--- extra/mongo_url.sh
+++ extra/mongo_url.sh
@@ -9,8 +9,13 @@
 echo "-----> Adding profile script to resolve MONGO_URL from mongolab addon"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/mongo_url.sh <<EOF
   #!/bin/bash
+
   if [ -z \$MONGO_URL ] ; then
-    export MONGO_URL=\${MONGODB_URI:-\${MONGOLAB_URI:-\$MONGOHQ_URL}}
+  echo "exporting MONGO_URL"
+
+    export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['credentials']['url'].start_with? 'mongodb' }[1][0]['credentials']['url']"`}
+
+ #   export MONGO_URL=\${MONGODB_URI:-\${MONGOLAB_URI:-\$MONGOHQ_URL}}
   fi
   if [ -z \$MONGO_URL ] ; then
     echo "meteor-buildpack-horse: MONGO_URL missing, you must define it for meteor to work."
--- extra/root_url.sh
+++ extra/root_url.sh
@@ -6,6 +6,8 @@ echo "-----> Adding profile script to resolve ROOT_URL from heroku app name"
 cat > "$APP_CHECKOUT_DIR"/.profile.d/root_url.sh <<EOF
   #!/bin/bash
   if [ -z "\$ROOT_URL" ] && [ -n "\$HEROKU_APP_NAME" ] ; then
-    export ROOT_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
+     export ROOT_URL=\${ROOT_URL:-`ruby -e 'require "json"; puts "http://#{JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"][0]}"'`}
+echo "exporting root url"
+   # export ROOT_URL="https://${HEROKU_APP_NAME}.herokuapp.com"
   fi
 EOF
